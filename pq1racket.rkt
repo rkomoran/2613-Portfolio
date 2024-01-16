1d82139884f223e5f92f7d91dc6e0bd2f03b8875
Rinor Komorani <103604250+rkomoran@users.noreply.github.com>
2024-01-16 19:46:15 -0400

    Update pq1racket.rkt

--- pq1racket.rkt
+++ pq1racket.rkt
@@ -4,12 +4,19 @@
 (colorize (filled-rectangle 40 40) "red")
 (disk 40)
 
-(define (my-rainbow shape)
-  (let ([colors '("red" "orange" "yellow" "green" "blue" "purple")])
-    (empty? colors) '()
-    (cons (colorize shape (first colors)) (my-rainbow (colorize shape (first (rest colors)))))))
+(define (helper s1 myList)
+  (if (empty? myList)
+  '()
+  (cons (colorize s1 (first myList)) (helper s1 (rest myList)))))
 
 
+(helper (disk 40) '("red" "orange" "yellow" "green" "blue" "purple"))
 
+;(define (my-rainbow2 shape)
+  ;(let ([colors '("red" "orange" "yellow" "green" "blue" "purple")])
+    ;(empty? colors) '()
+    ;(cons (colorize shape (first colors)) (my-rainbow (colorize shape (first (rest colors)))))))
 
-  
+;;(define (my-rainbow s1)
+  ;(my-rainbow-helper s1 '("red" "orange" "yellow" "green" "blue" "purple")))
+
