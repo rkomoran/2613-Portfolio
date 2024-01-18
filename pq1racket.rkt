d3d6cb18c0454c1b83f108f667624cb42245dc4a
Rinor Komorani <103604250+rkomoran@users.noreply.github.com>
2024-01-18 15:30:26 -0400

    Update pq1racket.rkt

--- pq1racket.rkt
+++ pq1racket.rkt
@@ -5,11 +5,12 @@
          '()]
         [(even? count)
          (cons (colorize s1 (first myList)) (rainbow-helper (sub1 count) s1 s2 (rest myList)))]
-        [(odd? count)
+        [else
          (cons (colorize s2 (first myList)) (rainbow-helper (sub1 count) s1 s2 (rest myList)))]))
 
 (define (my-rainbow s1 s2)
-  (let ([size (length '("red" "orange" "yellow" "green" "blue" "purple"))])
-    (rainbow-helper size s1 s2 '("red" "orange" "yellow" "green" "blue" "purple"))))
+  (let* ([colors (list "red" "orange" "yellow" "green" "blue" "purple")]
+        [size (length colors)])
+    (rainbow-helper size s1 s2 colors)))
 
 (my-rainbow (filled-rectangle 25 25) (filled-ellipse 25 25))
