09a8de607616ef67b92ea97e768d2c777a20d06f
Rinor Komorani <103604250+rkomoran@users.noreply.github.com>
2024-03-06 15:02:34 -0400

    Update PQOctave3.m

--- Programming Questions/Octave/PQOctave3.m
+++ Programming Questions/Octave/PQOctave3.m
@@ -1,6 +1,6 @@
 disp("Running...");
 function freq = readFreq(filename)
-  freq = struct(); % Initialize empty struct
+  freq = struct(); 
 
   inputFile = fopen(filename, "r");
 
@@ -28,10 +28,45 @@ function freq = readFreq(filename)
 end
 
 histogramBeowulf = readFreq('Beowulf.txt');
+% fieldnames retuns a cell array of strings with the names of the fields 
+% disp(length(fieldnames(histogramBeowulf)))
+% disp(histogramBeowulf)
 histogramVindication = readFreq('Vindication.txt');
+% disp(length(fieldnames(histogramVindication)))
 
-function determineOrigin = result(histogram1, histogram2, line)
+function retval = determineOrigin(histogram1, histogram2, line)
+    retVal = 0;
+    histogram1Length = length(fieldnames(histogram1));
+    histogram2Length = length(fieldnames(histogram2));
+    pHistogram1 = 0;
+    pHistogram2 = 0;
 
+    % want to split line into indivual words
+    words = strsplit(line);
+
+    % need to check how many times that word (if it's in the histogram) appears
+    % and divide it by the total words in that histogram
+    for i = 1:length(words)
+        % extract word
+        word = words{i};
+  
+        % check if word exists as a field in freq struct
+        if isfield(histogram1, word)
+            % need to check the count of that word
+            pHistogram1 = pHistogram1 + (histogram1.(word)/histogram1Length);
+        endif
+
+        if isfield(histogram2, word)
+            % need to check the count of that word
+            pHistogram2 = pHistogram2 + (histogram2.(word)/histogram2Length);
+        endif
+    endfor
+
+    if (pHistogram1 > pHistogram2)
+        retVal = pHistogram1;
+    else 
+        retVal = pHistogram2;
+    endif
 end
 
-
+determineOrigin(histogramBeowulf, histogramVindication, "This is a test");
