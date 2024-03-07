a167cf6736fb37e1a599a4cd2d03412c37f3e46a
Rinor Komorani <103604250+rkomoran@users.noreply.github.com>
2024-03-06 22:56:09 -0400

    Update PQOctave3.m

--- Programming Questions/Octave/PQOctave3.m
+++ Programming Questions/Octave/PQOctave3.m
@@ -1,6 +1,6 @@
 disp("Running...");
 function freq = readFreq(filename)
-  freq = struct(); 
+  freq = struct();
 
   inputFile = fopen(filename, "r");
 
@@ -27,48 +27,46 @@ function freq = readFreq(filename)
   fclose(inputFile);
 end
 
-histogramBeowulf = readFreq('Beowulf.txt');
-% fieldnames retuns a cell array of strings with the names of the fields 
-% disp(length(fieldnames(histogramBeowulf)))
-% disp(histogramBeowulf)
-histogramVindication = readFreq('Vindication.txt');
-% disp(length(fieldnames(histogramVindication)))
+function retval = naiveBayes(histogram, words)
+  retval = 0;
+  % fieldnames returns a cell array of strings with the names of the fields
+  histogramLength = length(fieldnames(histogram));
+
+  for i = 1:length(words)
+    word = words{i};
+
+    % need to check how many times that word (if it's in the histogram) appears
+    % and divide it by the total words in that histogram
+    if isfield(histogram, word)
+      retval = retval + (histogram.(word)/histogramLength);
+    endif
+
+  endfor
+end
 
 function retval = determineOrigin(histogram1, histogram2, line)
     retVal = 0;
-    histogram1Length = length(fieldnames(histogram1));
-    histogram2Length = length(fieldnames(histogram2));
     pHistogram1 = 0;
     pHistogram2 = 0;
 
     % want to split line into indivual words
     words = strsplit(line);
 
-    % need to check how many times that word (if it's in the histogram) appears
-    % and divide it by the total words in that histogram
-    for i = 1:length(words)
-        % extract word
-        word = words{i};
-  
-        % check if word exists as a field in freq struct
-        if isfield(histogram1, word)
-            % need to check the count of that word
-            pHistogram1 = pHistogram1 + (histogram1.(word)/histogram1Length);
-        endif
-
-        if isfield(histogram2, word)
-            % need to check the count of that word
-            pHistogram2 = pHistogram2 + (histogram2.(word)/histogram2Length);
-        endif
-    endfor
+    pHistogram1 = naiveBayes(histogram1, words);
+    pHistogram2 = naiveBayes(histogram2, words);
 
     if (pHistogram1 > pHistogram2)
-        disp("This is likely histogram1");
+        disp("More likely to be written by Mary Wollstonecraft");
         retVal = pHistogram1
-    else 
-        disp("This is likely histogram2");
+    elseif (pHistogram2 > pHistogram1)
+        disp("More likely to be written by Unknown Author");
         retVal = pHistogram2
+    else
+        disp("Could be either author");
     endif
 end
 
-determineOrigin(histogramBeowulf, histogramVindication, "Forth he fared at the fated moment,");
+histogramBeowulf = readFreq('Beowulf.txt');
+histogramVindication = readFreq('Vindication.txt');
+
+determineOrigin(histogramVindication, histogramBeowulf, "Forth he fared at the fated moment,");
